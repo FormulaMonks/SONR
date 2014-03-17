7cf5812a0bd97967d59789b2ad0835b1098e2b80
John Mark Schofield <jms@schof.org>
2014-03-17 11:24:29 -0700

    Fully set-up start, stop script.

--- unicorn.rb
+++ unicorn.rb
@@ -8,10 +8,10 @@ timeout 30
 
 # Specify path to socket unicorn listens to,
 # we will use this in our nginx.conf later
-listen "#{@dir}tmp/sockets/unicorn.sock", :backlog => 64
+listen "#{@dir}sockets/unicorn.sock", :backlog => 64
 
 # Set process id path
-pid "#{@dir}tmp/pids/unicorn.pid"
+pid "#{@dir}pids/unicorn.pid"
 
 # Set log file paths
 stderr_path "#{@dir}log/unicorn.stderr.log"
