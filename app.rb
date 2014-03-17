3b1d8f12394fe4a444360c59cb015c1d058bd39c
John Mark Schofield <jms@schof.org>
2014-03-17 12:23:35 -0700

    Experimenting with moving app.rb configure block. Also remove unneeded 'starting nginx' statement from start and stop scripts.

--- app.rb
+++ app.rb
@@ -1,13 +1,6 @@
 require 'sinatra'
 require 'redis'
 
-configure do
-    REDIS_HOST = ENV['OPENSHIFT_REDIS_HOST']
-    REDIS_PORT = ENV['OPENSHIFT_REDIS_PORT']
-    REDIS_PW = ENV['REDIS_PASSWORD']
-    puts "Connecting to redis on host #{REDIS_HOST}, port #{REDIS_PORT}."
-    REDIS = Redis.new(:host => REDIS_HOST, :port => REDIS_PORT, :password => REDIS_PW)
-end
 
 
 get '/' do
@@ -22,8 +15,13 @@ get '/agent' do
 end
 
 get '/count' do
-    REDIS.incr("count")
-    count = REDIS.get("count").to_s
-    "This page has been loaded #{count} times<br />
-    Return to <a href=\"/\">top</a>"
+  REDIS_HOST = ENV['OPENSHIFT_REDIS_HOST']
+  REDIS_PORT = ENV['OPENSHIFT_REDIS_PORT']
+  REDIS_PW = ENV['REDIS_PASSWORD']
+  puts "Connecting to redis on host #{REDIS_HOST}, port #{REDIS_PORT}."
+  REDIS = Redis.new(:host => REDIS_HOST, :port => REDIS_PORT, :password => REDIS_PW)
+  REDIS.incr("count")
+  count = REDIS.get("count").to_s
+  "This page has been loaded #{count} times<br />
+  Return to <a href=\"/\">top</a>"
 end
